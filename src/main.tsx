31e8f7473ebd1fd57c583f718c361b389041f344
박주호 <parkjh001217@gmail.com>
2025-03-11 19:31:05 +0900

    feat: 기본 레이아웃 작성

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@routes/routes.tsx';
+import { Globalstyles } from '@styles/Globalstyles.ts';
+import * as Styled from '@styles/BaseStyle.styled.ts';
+import { Footer, MainContainer } from '@styles/BaseStyle.styled.ts';
+import TitleBox from '@components/common/TitleBox.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Globalstyles />
+    <Styled.Base>
+      <Styled.BackGroundContainer>
+        <Styled.MainContainer>
+          <TitleBox />
+          <RouterProvider router={router} />
+        </Styled.MainContainer>
+      </Styled.BackGroundContainer>
+      <Footer />
+    </Styled.Base>
   </StrictMode>,
 );
