12cc83c348eb97a0d213fa776eb473fb5e85d987
박주호 <parkjh001217@gmail.com>
2025-03-13 00:15:16 +0900

    docs: 공용 스타일 경로 변경에 따른 import 경로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@routes/routes.tsx';
-import { Globalstyles } from '@styles/Globalstyles.ts';
-import * as Styled from '@styles/BaseStyle.styled.ts';
+import { Globalstyles } from '@styles/commonStyle/Globalstyles.ts';
+import * as Styled from '@styles/commonStyle/BaseStyle.styled.ts';
 import TitleBox from '@components/common/TitleBox.tsx';
 
 createRoot(document.getElementById('root')!).render(
