f38229ed7ac67181997f207477c4fd35b1b43894
박주호 <parkjh001217@gmail.com>
2025-03-12 17:09:51 +0900

    feat: 사용자 상호작용 여부에 따라 깜빡이는 애니메이션 추가

--- src/components/domain/DirectivePage/Answer.tsx
+++ src/components/domain/DirectivePage/Answer.tsx
@@ -2,11 +2,16 @@ import styled from 'styled-components';
 import React, { useEffect } from 'react';
 import { COLORS } from '@utils/color.ts';
 import { useAnswerStore } from '@/store/answerStore.ts';
+import { userAfkStore } from '@/store/userAfkStore.ts';
 
 interface AnswerProps {
   selectedWords: string[];
   onDrop: (index: number) => (e: React.DragEvent<HTMLDivElement>) => void;
   onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
+}
+
+interface AnswerDragBoxStyleProps {
+  blink?: boolean;
 }
 
 const AnswerField = styled.div`
@@ -33,13 +38,23 @@ const AnswerBox = styled.div`
   }
 `;
 
-const AnswerDragBox = styled.p`
+const AnswerDragBox = styled.p.withConfig({
+  shouldForwardProp: (prop) => !['blink'].includes(prop),
+})<AnswerDragBoxStyleProps>`
   background-color: white;
   width: 100px;
   padding: 20px;
   border-radius: 10px;
   color: ${COLORS.MAIN};
   text-align: center;
+
+  ${(props) => props.blink && `animation: blink-effect 1s step-end infinite`};
+
+  @keyframes blink-effect {
+    50% {
+      opacity: 0;
+    }
+  }
 `;
 
 export default function Answer({
@@ -47,7 +62,17 @@ export default function Answer({
   onDrop,
   onDragOver,
 }: AnswerProps) {
-  const { setAnswer } = useAnswerStore();
+  const { isAfk, setIsAfk } = userAfkStore();
+  const { answer, setAnswer } = useAnswerStore();
+
+  useEffect(() => {
+    setIsAfk(false);
+    const timer = setTimeout(() => {
+      setIsAfk(true);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [selectedWords]);
 
   useEffect(() => {
     setAnswer(selectedWords);
@@ -57,15 +82,27 @@ export default function Answer({
     <>
       <AnswerField />
       <AnswerBox>
-        <AnswerDragBox onDrop={onDrop(0)} onDragOver={onDragOver}>
+        <AnswerDragBox
+          onDrop={onDrop(0)}
+          onDragOver={onDragOver}
+          blink={isAfk && selectedWords[0] !== answer[0]}
+        >
           {selectedWords[0] || ''}
         </AnswerDragBox>
         <p>색의</p>
-        <AnswerDragBox onDrop={onDrop(1)} onDragOver={onDragOver}>
+        <AnswerDragBox
+          onDrop={onDrop(1)}
+          onDragOver={onDragOver}
+          blink={isAfk && selectedWords[1] !== answer[1]}
+        >
           {selectedWords[1] || ''}
         </AnswerDragBox>
         <p>표정을 하고 있는</p>
-        <AnswerDragBox onDrop={onDrop(2)} onDragOver={onDragOver}>
+        <AnswerDragBox
+          onDrop={onDrop(2)}
+          onDragOver={onDragOver}
+          blink={isAfk && selectedWords[2] !== answer[2]}
+        >
           {selectedWords[2] || ''}
         </AnswerDragBox>
         <p>그림을 그려줘</p>
