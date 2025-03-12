e16d360d11f0d2d7c89793d078472f0ea71b419d
박주호 <parkjh001217@gmail.com>
2025-03-13 00:07:34 +0900

    refactor: 코드 분할 및 스타일 분리

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,39 +1,15 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import Button from '@components/common/Button.tsx';
 import { COLORS } from '@utils/color.ts';
+import * as Styled from '@styles/component/common/Pagination.styled';
 
 interface Props {
-  color?: string;
+  currentPage: number;
+  gotoPage: (page: number) => void;
 }
 
-const PaginationBox = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-  position: absolute;
-  bottom: 25px;
-  left: 0;
-  padding: 0 20px 0 20px;
-`;
-
-const PageBox = styled.div`
-  display: flex;
-  gap: 5px;
-`;
-
-const Text = styled.p<Props>`
-  color: ${(props) => props.color || `${COLORS.MAIN}`};
-  font-weight: bold;
-  font-size: large;
-  display: flex;
-  align-items: center;
-  justify-items: center;
-  gap: 4px;
-`;
-
+// 페이지 네이션 컴포넌트
 export default function Pagination() {
   const navigate = useNavigate();
   const { page } = useParams();
@@ -44,35 +20,60 @@ export default function Pagination() {
   };
 
   return (
-    <PaginationBox>
-      <div>
-        <Button
-          border="none"
-          disabled={currentPage === 1}
-          onClick={() => gotoPage(Number(currentPage - 1))}
+    <Styled.PaginationBox>
+      <PrevButton currentPage={currentPage} gotoPage={gotoPage} />
+      <PageNumber currentPage={currentPage} />
+      <NextButton currentPage={currentPage} gotoPage={gotoPage} />
+    </Styled.PaginationBox>
+  );
+}
+
+// 이전 버튼
+function PrevButton({ currentPage, gotoPage }: Props) {
+  return (
+    <div>
+      <Button
+        border="none"
+        disabled={currentPage === 1}
+        onClick={() => gotoPage(Number(currentPage - 1))}
+      >
+        <Styled.Text
+          color={`${currentPage === 1 ? `${COLORS.DISABLE_COLOR}` : ''}`}
         >
-          <Text color={`${currentPage === 1 ? `${COLORS.DISABLE_COLOR}` : ''}`}>
-            <IoIosArrowBack />
-            이전
-          </Text>
-        </Button>
-      </div>
-      <PageBox>
-        <Text>{currentPage}</Text>
-        <Text color={`${COLORS.DISABLE_COLOR}`}>/</Text>
-        <Text color={`${COLORS.DISABLE_COLOR}`}>3</Text>
-      </PageBox>
-      <div>
-        <Button
-          border="none"
-          disabled={currentPage === 3}
-          onClick={() => gotoPage(Number(currentPage + 1))}
+          <IoIosArrowBack />
+          이전
+        </Styled.Text>
+      </Button>
+    </div>
+  );
+}
+
+// 페이지 넘버
+function PageNumber({ currentPage }: { currentPage: number }) {
+  return (
+    <Styled.PageBox>
+      <Styled.Text>{currentPage}</Styled.Text>
+      <Styled.Text color={`${COLORS.DISABLE_COLOR}`}>/</Styled.Text>
+      <Styled.Text color={`${COLORS.DISABLE_COLOR}`}>3</Styled.Text>
+    </Styled.PageBox>
+  );
+}
+
+// 다음 버튼
+function NextButton({ currentPage, gotoPage }: Props) {
+  return (
+    <div>
+      <Button
+        border="none"
+        disabled={currentPage === 3}
+        onClick={() => gotoPage(Number(currentPage + 1))}
+      >
+        <Styled.Text
+          color={`${currentPage === 3 ? `${COLORS.DISABLE_COLOR}` : ''}`}
         >
-          <Text color={`${currentPage === 3 ? `${COLORS.DISABLE_COLOR}` : ''}`}>
-            다음 <IoIosArrowForward />
-          </Text>
-        </Button>
-      </div>
-    </PaginationBox>
+          다음 <IoIosArrowForward />
+        </Styled.Text>
+      </Button>
+    </div>
   );
 }
